1d0c75289ead88152ca54f9877ef94321a52edaf
seohyun319 <tjguselly0319@gmail.com>
2021-10-11 00:29:18 +0900

    [1주차] REACT 시작하기 & JSX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App" style={{backgroundColor:"black", padding:"16px", color:"#61DAFB", fontSize:"50px"}}>
+      리액트 JSX 공부 인증 (김서현)
+    </div>
+  );
 }
 
 export default App;
