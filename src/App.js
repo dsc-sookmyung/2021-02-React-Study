83b01307d2a48e46dfdd2eeef635e133310d4404
Mingadinga <minpearl0826@gmail.com>
2021-10-10 19:14:17 +0900

    [2주차] state, event

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Title from './component/Title';
+import Title_btn from './component/Title_btn';
+import React, {Component} from 'react';
+import { useState } from 'react';
+
 function App() {
-  return <div className="App">Hello</div>;
+
+  var styles={paddingLeft: "50px"};
+  const [title, setTitle] = useState('민휘');
+
+  return (
+    <div style={styles}>
+      <Title content={title}/>
+      <Title_btn 
+        onChangePage = {function(){
+          setTitle(title === '민휘'?'리액트 스터디 화이팅!':'민휘');
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
