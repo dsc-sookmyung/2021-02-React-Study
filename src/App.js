d7625b59121aaa27987f6481c442094f14c88b5f
heleneunji <heleneunji@gmail.com>
2021-10-11 06:03:53 +0900

    [2주차] event & useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import {useState} from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [title, setTitle] = useState("권은지");
+
+  function change() {
+    const newTitle = title === "권은지" ? "리액트 스터디 화이팅!" : "권은지";
+    console.log(title);
+    //document.getElementById("name").innerText = name;
+    setTitle(newTitle);
+  }
+
+  return (
+    <div className="App">
+      <h1 id="title">{title}</h1>
+      <button onClick={change}>타이틀 바꾸기</button>
+    </div>
+  );
 }
 
 export default App;
