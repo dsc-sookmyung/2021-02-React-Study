ea503f07e51965499e06b06ad39c3a967a01bbe4
KIM MINJI <sml07113@gmail.com>
2021-10-18 02:10:44 +0900

    [3주차] component & props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import Fighting from "./component/Fighting";
+import { useState } from "react";
+import Props from "./component/Props";
+import "./App.css";
 
 function App() {
 
+  let [num, setNum] = useState(0); // 변수 num의 초기값 = 0
+  
+  function increase() {
+    setNum(++num); // 먼저 연산 후 num 값 저장
+  }
+
+  function decrease() {
+    setNum(--num);
+  }
+
   return (
     <div className="App">
-      {/* 컴포넌트 사용 */}
-      <Fighting />
+      <button onClick={ increase } className="btn">increase</button>
+      <button onClick={ decrease } className="btn">decrease</button>
+      {/* num 값을 Props 컴포넌트에 전달 */}
+      <Props num={num} />
     </div>
   );
 }
--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,39 +1,21 @@
 import { useState } from "react";
-import styles from "./Hello.module.css";
+import UserName from "./UserName";
 
-function showName() {
-  console.log("Mike");
-}
-
-function showAge(age) {
-  console.log(age);
-}
-
-function showText(txt) {
-  console.log(txt);
-}
-
-export default function Hello() {
+export default function Hello(props) {
+  console.log(props);
   const [name, setName] = useState('mike');
+  const [age, setAge] = useState(props.age);
+  const msg = age > 19 ? "성인" : "미성년자";
 
   function changeName() {
     setName(name === 'mike' ? 'jane' : 'mike');
+    setAge(age + 1);
   }
 
   return (
     <div>
-      <h1 className={ styles.box }>Hello</h1>
-      <button onClick={ showName }>Show name</button>
-      <button onClick={() => {
-          showAge(20);
-      }}>Show age</button>
-      <input type="text" onChange={ e => {
-        const txt = e.target.value;
-        showText(txt);
-      } } />
-
-      <h1>state</h1>
-      <h2>{ name }</h2>
+      <h2>{ name }({age} 세) : {msg}</h2>
+      <UserName name={name} / >
       <button onClick={ changeName }>Change</button>
     </div>
   );
