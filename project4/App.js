8f3b0e946a4810be64399e5e75d0f109c7834c7a
zhenying2 <khappy9888@sookmyung.ac.kr>
2021-10-31 20:04:21 +0900

    4주차 react-router-dom

---
+++ project4/App.js
@@ -0,0 +1,23 @@
+import {Link, BrowserRouter, Route, Switch} from "react-router-dom";
+
+function App() {
+  return (
+  <BrowserRouter>
+  <div className="App">
+
+    <Switch>
+      <Route exact path="/">
+        <h1>This is Comp 1 page</h1>
+        <Link to={'/a'}>link to /a</Link>
+      </Route>
+      <Route exact path="/a">
+        <h1>This is Comp 2 page</h1>
+        <Link to={'/'}>link to /</Link>
+      </Route>
+    </Switch>
+  </div>
+  </BrowserRouter>
+  );
+}
+
+export default App;
