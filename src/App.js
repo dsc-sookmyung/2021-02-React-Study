a3f7ae5f4fe5034149acea20c037a9e3f57deb49
jihye <asd36384@gmail.com>
2021-11-01 01:00:51 +0900

    3_week_hw_check

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {  
+function App() {
   return (
     <BrowserRouter>
-      <div className="App">      
+      <div className="App">
         <Switch>
-          <Route exact path="/"> 
+          <Route exact path="/">
             <div>This is Comp 1 page</div>
-            <Link to={'/a'}>link to /a</Link>
+            <Link to={"/a"}>link to /a</Link>
           </Route>
           <Route path="/a">
             <div>This is Comp 2 page</div>
-            <Link to={'/'}>link to /</Link>
+            <Link to={"/"}>link to /</Link>
           </Route>
         </Switch>
       </div>
