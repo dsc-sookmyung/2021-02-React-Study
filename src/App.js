a0747b1297f12adf24fe114e7856956b940005f3
seohyun319 <tjguselly0319@gmail.com>
2021-10-31 23:04:39 +0900

    [4주차] react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import { useState } from 'react';
-import Props from './component/Props';
+import { Link } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {  
-  const [num, setNum] = useState(0)
-    
-  function plus() {
-      setNum(num + 1);
-  }
-  function minus() {
-      setNum(num - 1);
-  }
-
   return (
-    <div className="App">      
-      <div>
-        <button onClick={plus}>increase</button>
-        <button onClick={minus}>decrease</button>
+    <BrowserRouter>
+      <div className="App">      
+        <Switch>
+          <Route exact path="/"> 
+            <div>This is Comp 1 page</div>
+            <Link to={'/a'}>link to /a</Link>
+          </Route>
+          <Route path="/a">
+            <div>This is Comp 2 page</div>
+            <Link to={'/'}>link to /</Link>
+          </Route>
+        </Switch>
       </div>
-      <Props num={num} />
-    </div>
+    </BrowserRouter>
   );
 }
 
