6d47bfbcdebf5fc3735f4c5ddfdf80b133c7292b
KIM MINJI <sml07113@gmail.com>
2021-10-31 23:24:50 +0900

    [4주차] react-router-dom 과제

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import "./App.css";
-import Header from "./component/Header";
-import DayList from "./component/DayList";
-import Day from "./component/Day";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import EmptyPage from "./component/EmptyPage";
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import Router from "./component/Router";
 
 function App() {
 
   return (
     <BrowserRouter>
       <div className="App">
-        <Header />
         <Switch>
           <Route exact path="/">
-            <DayList />
+            <p>This is Comp 1 page</p>
+            <Link to="/a">link to /a</Link>
           </Route>
-          <Route path="/day/:day"> {/* 변수 day */}
-            <Day />
-          </Route>
-          <Route> {/* 어떤 조건도 만족하지 않을 때 */}
-            <EmptyPage />
+          <Route path="/a">
+            <Router />
           </Route>
         </Switch>
       </div>
---
+++ src/component/Router.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Router() {
+  return (
+    <>
+      <p>This is Comp 2 page</p>
+      <Link to="/">link to /</Link>
+    </>
+  );
+}
