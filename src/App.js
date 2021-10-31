cca64eb9e380f681fbbc260cceadc7c616aa1942
jiae yoon <yoon529@sookmyung.ac.kr>
2021-10-31 11:03:07 +0900

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import AddSub from './AddSub';
+import Main from './component/Main';
+import Page2 from './component/Page2'
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <AddSub num={0} />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+            <Route exact path="/">
+              <Main/>
+            </Route>
+            <Route path="/a">
+              <Page2 />
+            </Route>
+          </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
