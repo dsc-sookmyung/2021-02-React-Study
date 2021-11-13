dbd16dcbb84aad0719ac4894336102071449204f
seohyun319 <tjguselly0319@gmail.com>
2021-11-13 11:57:40 +0900

    [5주차] react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import { Link } from "react-router-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from "react";
+import Inflearn from "./component/Inflearn";
 
-function App() {  
+function App() {
   return (
-    <BrowserRouter>
-      <div className="App">      
-        <Switch>
-          <Route exact path="/"> 
-            <div>This is Comp 1 page</div>
-            <Link to={'/a'}>link to /a</Link>
-          </Route>
-          <Route path="/a">
-            <div>This is Comp 2 page</div>
-            <Link to={'/'}>link to /</Link>
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <Inflearn />
   );
 }
 
